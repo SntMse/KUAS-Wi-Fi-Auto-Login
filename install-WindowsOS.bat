b45f13acb8c180cc25a7c70f6d8a8cf022ee87cb
Shintaro Muraseh <64265556+SntMse@users.noreply.github.com>
2026-07-24 20:17:28 +0900

    Update PowerShell script call in installer batch file

--- install-WindowsOS.bat
+++ install-WindowsOS.bat
@@ -4,6 +4,6 @@ title KUAS Wi-Fi Auto Login Installer
 echo KUAS Wi-Fi自動ログイン (Windows版) のインストーラーを起動しています...
 
 :: PowerShellをセキュリティ制限を無視して起動し、インストーラーを呼び出す
-powershell -NoProfile -ExecutionPolicy Bypass -Command "& '%~dp0install-WindowsOS.ps1'"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& '%~dp0install-WindowsOS-script.ps1'"
 
 pause
